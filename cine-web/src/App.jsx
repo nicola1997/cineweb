8c1069381120520da832249868907c33df9fb038
ndipinto <nicola.dipinto@dxc.com>
2024-10-07 09:55:27 +0200

    add bootstrap

--- cine-web/src/App.jsx
+++ cine-web/src/App.jsx
@@ -1,29 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {useState} from 'react'
 import './App.css'
+import EuroInput from "./components/EuroInput.jsx";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBarCine from "./components/NavBarCine.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count  {count}
-        </button>
-      </div>
-    </>
-  )
+    return (
+        <>
+            <NavBarCine></NavBarCine>
+            <EuroInput></EuroInput>
+
+        </>
+    )
 }
 
 export default App
